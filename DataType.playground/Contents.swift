fed0941cb8c01b8831aae861ac63aae5806c10af
Jason <jusong23@naver.com>
2022-03-21 17:57:08 +0900

    조건문 실습

--- DataType.playground/Contents.swift
+++ DataType.playground/Contents.swift
@@ -1,9 +1,28 @@
+import Foundation
 import UIKit
 
-var greeting = "Hello, playground"
+var isChecked = false
 
-var isChecked: Bool = false
-isChecked = true
+if isChecked {
+    print("체크되어 있슴")
+} else {
+    print("체크 안돼써")
+}
 
-var test = 100
-type(of: test)
+var time = 10
+if time == 9 {
+    print("저녁먹을 시간")
+} else if time == 12 {
+    print("점심먹을 시간")
+} else { print("자유시간")
+}
+
+func getName(name: String?) -> String {
+    guard let uName = name else {
+        return "이름값이 존재하지 않습니다"
+    }
+    return uName
+}
+
+getName(name: "Jason")
+getName(name: nil)
