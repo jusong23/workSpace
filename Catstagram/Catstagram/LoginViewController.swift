59b00c51c314753ca9addc139bac0ae16e8904df
Jason <jusong23@naver.com>
2022-04-02 21:33:50 +0900

    노답임
    처참히 멸망 직전에 복구완료

--- Catstagram/Catstagram/LoginViewController.swift
+++ Catstagram/Catstagram/LoginViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by 이주송 on 2022/03/24.
 //
-
 import UIKit
 
 class LoginViewController: UIViewController {
@@ -16,31 +15,35 @@ class LoginViewController: UIViewController {
 
     @IBOutlet var loginButton: UIButton!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        setupAttribute()
+    }
+    
     @IBAction func emailTextFieldEditingChanged(_ sender: UITextField) {
         let text = sender.text ?? ""
         // 조건 ?? 실행문 : 조건이 거짓일때만 ""를 실행한다는 뜻
         // 값이 없을 때(비어있을 때)는 "" 가 존재하게 하여 Wrapping
-        loginButton.backgroundColor = text.isVaildEmail() ? .facebookColor : .disabledButtonColor
-        email = text
+        self.loginButton.backgroundColor = text.isValidEmail() ? .facebookColor : .disabledButtonColor
+        self.email = text
     } // 입력할 때마다 호출 되는 함수
-
     @IBAction func passwordTextFieldEditingChanged(_ sender: UITextField) {
         let text = sender.text ?? ""
-        loginButton.backgroundColor = text.count > 2 ? .facebookColor : .disabledButtonColor
+        self.loginButton.backgroundColor = text.count > 2 ? .facebookColor : .disabledButtonColor
 
-        password = text
+        self.password = text
     }
 
     @IBAction func loginButtonDidTap(_: UIButton) {
-        // 회원가입 정보를 전달받아서, 그것과 textField 데이터가 일치하면
+        // 회원가입 정보를 전달받아서, 그것과 textField 데이터가 일치하면u
         // 로그인이 되어야한다.
-        guard let userInfo = userInfo else {
+        guard let userInfo = self.userInfo else {
             return
         } // Data 있으면 userInfo에 저장, 없으면 return으로 종료
-        if userInfo.email == email, userInfo.password == password {
+        if userInfo.email == self.email, userInfo.password == self.password {
             let vc = storyboard?.instantiateViewController(withIdentifier: "TabBarVC") as! UITabBarController
             vc.modalPresentationStyle = .fullScreen
-            present(vc, animated: true, completion: nil)
+            self.present(vc, animated: true, completion: nil)
         } else {}
     }
 
@@ -51,19 +54,15 @@ class LoginViewController: UIViewController {
         // 2. 뷰 컨트롤러를 생성
         let registerViewController = storyboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterViewController
         // 3. 화면전환 메소드를 이용해서 화면을 전환
-        navigationController?.pushViewController(registerViewController, animated: true)
+        self.navigationController?.pushViewController(registerViewController, animated: true)
         // self.present(registerViewController, animated: true, completion: nil)
-
         registerViewController.userInfo = { [weak self] userInfo in self?.userInfo = userInfo }
         // ARC -> 강한참조 / 약한참조 -> ARC 낮춰줌
         // [weak self]의 유무 강,약참조 -> 메모리 해지되면 해당 line도 삭제,
         // but 안쓰면(강한참조 시키면) 뷰컨트롤러 해지 돼도 해당 line이 계속 남아있어 메모리 낭비 유발
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-//        setupAttribute()
-    }
+
 
 //    private func setupAttribute() {
 //        // register button
--- Catstagram/Catstagram/RegisterViewController.swift
+++ Catstagram/Catstagram/RegisterViewController.swift
@@ -4,12 +4,10 @@
 //
 //  Created by 이주송 on 2022/03/26.
 //
-
 import UIKit
 
 class RegisterViewController: UIViewController {
     // MARK: - Properties
-
     var email: String = ""
     var name: String = ""
     var nickName: String = ""
@@ -20,31 +18,34 @@ class RegisterViewController: UIViewController {
     var isValidEamil = false {
         didSet {
             // 프로퍼티 옵저버 : 세팅이 되고 난 후 아래 코드 실행 !
-            // isVaildEmail로 입력 받을 때마다 아래 메소드가 실행
-            validUserInfo()
+            // isValidEmail로 입력 받을 때마다 아래 메소드가 실행
+            self.validUserInfo()
         }
     }
 
-    var isVaildName = false {
+    var isValidName = false {
         didSet {
-            validUserInfo()
+            self.validUserInfo()
         }
     }
 
-    var isVaildNickName = false {
+    var isValidNickName = false {
         didSet {
-            validUserInfo()
+            self.validUserInfo()
         }
     }
 
-    var isVaildPassword = false {
+    var isValidPassword = false {
         didSet {
-            validUserInfo()
+            self.validUserInfo()
         }
+    }
+    
+    var textFields: [UITextField] {
+        [emailTextField, nameTextField, nicknameTextField, passwordTextField]
     }
 
     // TextFields
-
     @IBOutlet var emailTextField: UITextField!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var nicknameTextField: UITextField!
@@ -52,38 +53,34 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet var signUpButton: UIButton!
 
-    var textFields: [UITextField] {
-        [emailTextField, nameTextField, nicknameTextField, passwordTextField]
-    }
-
-    // MARK: - Lfiecyce
-
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextField()
         // bug fix
-        navigationController?.interactivePopGestureRecognizer?.delegate = nil
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = nil
     }
+   
+    // MARK: - Lfiecyce
+
 
     // MARK: - ACtion
-
     @objc func textFieldEditingChanged(_ sender: UITextField) {
         // sender 타입의 UITextFiled를 인수로 받겠다는 것.
         let text = sender.text ?? ""
 
         switch sender {
         case emailTextField:
-            isValidEamil = text.isVaildEmail()
-            email = text
+            self.isValidEamil = text.isValidEmail()
+            self.email = text
         case nameTextField:
-            isVaildName = text.count > 2
-            name = text
+            self.isValidName = text.count > 2
+            self.name = text
         case nicknameTextField:
-            isVaildNickName = text.count > 2
-            nickName = text
+            self.isValidNickName = text.count > 2
+            self.nickName = text
         case passwordTextField:
-            isVaildPassword = text.isVaildPassword()
-            password = text
+            self.isValidPassword = text.isValidPassword()
+            self.password = text
         default:
             fatalError("Missing TextField")
         }
@@ -98,16 +95,15 @@ class RegisterViewController: UIViewController {
         navigationController?.popViewController(animated: true)
 
         let userInfo = UserInfo(
-            email: email,
-            name: name,
-            nickName: nickName,
-            password: password
+            email: self.email,
+            name: self.name,
+            nickName: self.nickName,
+            password: self.password
         )
         self.userInfo?(userInfo)
     }
 
     // MARK: - Helpers
-
     private func setupTextField() {
         textFields.forEach {
             txfd in txfd.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
@@ -117,9 +113,9 @@ class RegisterViewController: UIViewController {
     // 사용자가 입력한 회원정보를 확인하고 -> UI 업데이트
     private func validUserInfo() {
         if isValidEamil,
-           isVaildName,
-           isVaildNickName,
-           isVaildPassword
+           isValidName,
+           isValidNickName,
+           isValidPassword
         {
             signUpButton.isEnabled = true
             UIView.animate(withDuration: 0.33) {
@@ -140,15 +136,15 @@ class RegisterViewController: UIViewController {
 // 정규표현식
 extension String {
     // 대문자, 소문자, 특수문자, 숫자 8자 이상일 때 -> true
-    func isVaildPassword() -> Bool {
+    func isValidPassword() -> Bool {
         let regularExpression =
-            "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}"
-        let passwordVaildation = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+            "(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-zu\\d$@$!%*?&]{8,}"
+        let passwordValidation = NSPredicate(format: "SELF MATCHES %@", regularExpression)
 
-        return passwordVaildation.evaluate(with: self)
+        return passwordValidation.evaluate(with: self)
     }
 
-    func isVaildEmail() -> Bool {
+    func isValidEmail() -> Bool {
         let emailRegEx =
             "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", emailRegEx)
