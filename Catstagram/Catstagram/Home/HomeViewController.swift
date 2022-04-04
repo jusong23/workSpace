e065f383daa4e4b374f3da6bbeb61b2ead121515
Jason <jusong23@naver.com>
2022-04-04 16:00:40 +0900

    Navi Bar & Table View 생성
    4/4

---
+++ Catstagram/Catstagram/Home/HomeViewController.swift
@@ -0,0 +1,50 @@
+//
+//  HomeViewController.swift
+//  Catstagram
+//
+//  Created by 이주송 on 2022/04/04.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController{
+    
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 }
+    // tableView 몇개로 할 지
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.backgroundColor = .blue
+        return cell
+    }
+    // 각 셀들의 특성
+    
+}
