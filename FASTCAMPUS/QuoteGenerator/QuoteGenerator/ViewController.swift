6ea35a53ce4e6cf70bdfa88b235c0817abbd8cc1
jusong23 <jusong23@naver.com>
2022-04-28 21:56:05 +0900

    Complete
    최초로 모든걸 이해한 프로젝트

--- FASTCAMPUS/QuoteGenerator/QuoteGenerator/ViewController.swift
+++ FASTCAMPUS/QuoteGenerator/QuoteGenerator/ViewController.swift
@@ -13,7 +13,12 @@ class ViewController: UIViewController {
     
     let quotes = [
     Quote(contents: "명언1", name: "이름1"),
-    Quote(contents: "명언2", name: "이름2")
+    Quote(contents: "명언2", name: "이름2"),
+    Quote(contents: "명언3", name: "이름3"),
+    Quote(contents: "명언4", name: "이름4"),
+    Quote(contents: "명언5", name: "이름5"),
+
+    
     ] // 구조체 파일(swift)에 선언된 struct instance의 저장 프로퍼티
     
     override func viewDidLoad() {
@@ -21,9 +26,12 @@ class ViewController: UIViewController {
     }
 
     @IBAction func tapQuoteGenerator(_ sender: Any) {
-        
+        let random = Int(arc4random_uniform(5)) // 0~4 사이의 난수를 발생
+        let quote = quotes[random]
+        self.quoteLabel.text = quote.contents
+        self.nameLabel.text = quote.name
     }
     
 }
 
-// 저장 프로퍼티 , 휴깅, 컴프레션 레지스턴스,
+// 저장 프로퍼티 , 휴깅, 컴프레션 레지스턴스, 난수발생
