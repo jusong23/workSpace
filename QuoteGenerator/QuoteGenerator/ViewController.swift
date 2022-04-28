ea678c65c0e53c79e7f7c51a60dbad736d47d521
jusong23 <jusong23@naver.com>
2022-04-28 21:33:33 +0900

    Hugging & Compression Resistance

---
+++ QuoteGenerator/QuoteGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuoteGenerator
+//
+//  Created by 이주송 on 2022/04/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
