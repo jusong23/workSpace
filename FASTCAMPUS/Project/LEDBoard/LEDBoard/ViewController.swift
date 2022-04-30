bc51e744452ef20219104b6e6be762d223ee9fc5
jusong23 <jusong23@naver.com>
2022-04-30 10:56:19 +0900

    Directory Modified

---
+++ FASTCAMPUS/Project/LEDBoard/LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDBoard
+//
+//  Created by 이주송 on 2022/04/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
