eb22d4bbcf0f349debbb989d2ddfb77cae3aa1d9
jusong23 <jusong23@naver.com>
2022-05-01 22:58:59 +0900

    The end of LEDBoard

--- FASTCAMPUS/Project/LEDBoard/LEDBoard/SettingViewController.swift
+++ FASTCAMPUS/Project/LEDBoard/LEDBoard/SettingViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
+protocol LEDBoardSettingDelegate: AnyObject {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor)
+} //전달할 값들의 변수 이름과 타입 설정하기
+
+
 class SettingViewController: UIViewController {
 
     @IBOutlet weak var textField: UITextField!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
+    
 
     @IBOutlet weak var yellowButton: UIButton!
     @IBOutlet weak var greenButton: UIButton!
@@ -22,28 +24,57 @@ class SettingViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var orangeButton: UIButton!
     
+    weak var delegate: LEDBoardSettingDelegate?
+    var ledText: String?
+    var textColor: UIColor = .yellow
+    var backgroundColor: UIColor = .black // 보내야 하니, 기본 값 초기화
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureView()
+    }
+    
+    private func configureView() {
+        if let ledText = self.ledText {
+            self.textField.text = ledText
+        } // if 문처럼 뭐랑뭐랑 비교한다는 게 아니라, 그냥 옵셔널된 ledText를 바인딩해준다고 이해하자
+        self.changeTextColor(color: self.textColor)
+        self.changeBackgroundColorButton(color: self.backgroundColor)
+    }
+   
+    
     @IBAction func tapTextColorButton(_ sender: UIButton) {
         if sender == self.yellowButton {
             self.changeTextColor(color: .yellow)
+            self.textColor = .yellow
         } else if sender == self.greenButton {
             self.changeTextColor(color: .green)
+            self.textColor = .green
         } else {
             self.changeTextColor(color: .purple)
+            self.textColor = .purple
         }
     } // 버튼을 누르게 되면 선택된 값이 sender에 전달
     
     @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
         if sender == self.blackButton {
             self.changeBackgroundColorButton(color: .black)
+            self.backgroundColor = .black
         } else if sender == self.blueButton {
             self.changeBackgroundColorButton(color: .blue)
+            self.backgroundColor = .blue
         } else {
             self.changeBackgroundColorButton(color: .orange)
+            self.backgroundColor = .orange
         }
     }
     
     @IBAction func tapSaveButton(_ sender: UIButton) {
-        
+        self.delegate?.changedSetting(
+            text: self.textField.text,
+            textColor: self.textColor,
+            backgroundColor: self.backgroundColor)
+        self.navigationController?.popViewController(animated: true)
     }
     
     private func changeTextColor(color: UIColor){
--- FASTCAMPUS/Project/LEDBoard/LEDBoard/ViewController.swift
+++ FASTCAMPUS/Project/LEDBoard/LEDBoard/ViewController.swift
@@ -7,14 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+    func changedSetting(text: String?, textColor: UIColor, backgroundColor: UIColor) {
+        if let text = text {
+            self.contentsLabel.text = text
+        }
+        self.contentsLabel.textColor = textColor
+        self.view.backgroundColor = backgroundColor
+    }
 
     @IBOutlet weak var contentsLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.contentsLabel.textColor = .yellow
     }
+    
 
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            // 현재 이 ViewController를 settingViewController에 위임시킨다.
+            settingViewController.ledText = self.contentsLabel.text
+            settingViewController.textColor = self.contentsLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .black
+        }
+    }
+    
 }
 
+// 코드로 데이터 전달 &
