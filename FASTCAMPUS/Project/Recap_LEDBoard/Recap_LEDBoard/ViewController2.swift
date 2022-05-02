6603fe3c888e866c41b7d87662d9ea05f059915e
jusong23 <jusong23@naver.com>
2022-05-02 21:07:22 +0900

    Updated

--- FASTCAMPUS/Project/Recap_LEDBoard/Recap_LEDBoard/ViewController2.swift
+++ FASTCAMPUS/Project/Recap_LEDBoard/Recap_LEDBoard/ViewController2.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController2: UIViewController,RandomSettingDelegate {
+class ViewController: UIViewController,RandomSettingDelegate {
     func changeSetting(text: String?, textColor: UIColor) {
         if let text = text {
             self.labelName.text = text
@@ -21,14 +21,16 @@ class ViewController2: UIViewController,RandomSettingDelegate {
         labelName.sizeToFit()
     }
     
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let settingViewController = segue.destination as?
             SettingViewController {
             settingViewController.delegate = self
             settingViewController.randomedText = self.labelName.text
-//            settingViewController.randomColorView.backgroundColor = self.labelName.textColor
-//            settingViewController.selectedColorName.backgroundColor =
-//                self.labelName.textColor
+            settingViewController.backgroundColor = self.labelName.textColor ?? .black
+            settingViewController.textColor = self.labelName.textColor ?? .black
+            // 현재 labelName의 text와 textColor를 settingVC의 backgroundColor, textColor에
+            // 저장할것이고, 이들은 settingVC 내부에서 초기화 되어야 mainVC(옵셔널 바인딩)에서 넘길수 있는 것이다.
         }
     }
 }
